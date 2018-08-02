07722dc8d4e68ace697158a26b4d0ceea0673fe3
Shubham Gupta <shubhamg@outlook.com>
2018-08-03 03:53:08 +0530

    Add tests for Tree Traversal

--- src/__test__/TreeTraversal.test.js
+++ src/__test__/TreeTraversal.test.js
@@ -3,7 +3,31 @@ import { shallow } from 'enzyme';
 import TreeTraversal from '../components/TreeTraversal.jsx';
 
 describe('<TreeTraversal />', () => {
+  const wrapper = shallow(<TreeTraversal />);
   it('renders TreeTraversal without crashing', () => {
-    shallow(<TreeTraversal />);
-  });
+    wrapper
+  })
+  it('playing is set false', ()=> {
+    expect(wrapper.state('isPlaying')).toEqual(false);
+  })
+  it('BFS Traversal start', ()=> {
+    let arr = ["34","23","92","12","04","16","09"];
+    wrapper.instance().bft();
+    expect(wrapper.state('list')).toEqual(arr);
+  })
+  it('InOrder Traversal start', ()=> {
+    let arr = ["12","23","16","04","09","34","92"];
+    wrapper.instance().inOrder();
+    expect(wrapper.state('list')).toEqual(arr);
+  })
+  it('PreOrder Traversal start', ()=> {
+    let arr = ["34", "23", "12", "04", "16", "09", "92"];
+    wrapper.instance().preOrder();
+    expect(wrapper.state('list')).toEqual(arr);
+  })
+  it('postOrder Traversal start', ()=> {
+    let arr = ["12","16","09","04","23","92","34"];
+    wrapper.instance().postOrder();
+    expect(wrapper.state('list')).toEqual(arr);
+  })
 });
